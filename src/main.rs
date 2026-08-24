f66b413de00ec86573f59c01902ab826d28feafa
subparr <subparr@tuta.io>
2026-08-24 07:32:16 +0300

    Intermediate, general structure 3/n

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use std::collections::HashMap;
 use nix::sys::epoll::{Epoll, EpollEvent, EpollCreateFlags};
 use nix::errno::Errno;
 use std::path::PathBuf;
+use nix::sys::signal::{SigSet, Signal, sigprocmask, SigmaskHow};
+use nix::sys::signalfd::{SignalFd, SfdFlags};
 mod config;
 use config::{Cfg, CfgEntry};
 
@@ -34,7 +36,7 @@ fn main() {
         process::exit(0);
     }
 
-    let inotify_wd = init_inotify(&cfg).unwrap_or_else(|err| {
+    let inotify_state = init_inotify(&cfg).unwrap_or_else(|err| {
             eprintln!("Inotify init/add_watch error: {err}"); 
             process::exit(1); 
     });
@@ -43,27 +45,34 @@ fn main() {
             eprintln!("Epoll init error: {err}"); 
             process::exit(1); 
     });
+    
+    let sighup_fd = init_sighup().unwrap_or_else(|err| {
+            eprintln!("Sighup init error: {err}"); 
+            process::exit(1); 
+    });
+    
+        
+    event_loop(inotify_state, epoll_fd);
 }
 
 
 
 
-fn event_loop(inotify_fd: Inotify, epoll_fd: Epoll){
+fn event_loop(inotify_state: InotifyState, epoll_fd: Epoll){
     loop{
-        
+           
     }
 }
 
 fn init_epoll() -> Result<Epoll, Errno>{
     let epoll_fd = Epoll::new(EpollCreateFlags::EPOLL_CLOEXEC)?;
-    
     Ok(epoll_fd)
 }
 
 
 fn init_inotify(cfg: &Cfg) -> Result<InotifyState, Errno>{
     let mut inotify_state = InotifyState{
-        inotify_fd: Inotify::init(InitFlags::IN_CLOEXEC | InitFlags::IN_NONBLOCK)?, //Errno EAGAIN for epoll
+        inotify_fd: Inotify::init(InitFlags::IN_CLOEXEC | InitFlags::IN_NONBLOCK)?, //CLOEXEC NONBLOCK
         wd_to_script: HashMap::new(),
     };
 
@@ -71,12 +80,23 @@ fn init_inotify(cfg: &Cfg) -> Result<InotifyState, Errno>{
         let cycle_wd = inotify_state.inotify_fd.
             add_watch(&entry.path, entry.events)?;
         inotify_state.wd_to_script.
-            insert(cycle_wd, entry.path.clone()); //clone burger shitcode
+           insert(cycle_wd, entry.path.clone()); //clone burger shitcode
     }
     Ok(inotify_state)
 }
 
-fn init_signal(){}
+fn init_sighup() -> Result<SignalFd, Errno> { //add struct for storing signalfds if several introduced
+    let mut mask = SigSet::empty();
+    mask.add(Signal::SIGHUP);
+    sigprocmask(SigmaskHow::SIG_BLOCK, Some(&mask), None)?; //clear before script exec
+    
+    let sighup_fd = SignalFd::with_flags(&mask, SfdFlags::SFD_CLOEXEC | SfdFlags::SFD_NONBLOCK)?;
+    Ok(sighup_fd)
+}
 
 
-fn reload(){}
+fn reload(sighup_fd: SignalFd) -> Result<>{
+   todo!() 
+}
+
+fn fire_script() -> Result,.{}
